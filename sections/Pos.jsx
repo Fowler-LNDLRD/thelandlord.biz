6f208737c157e0124ba66d805a4df3d94ddf6a99
FrankHodler <frankhodler@gmail.com>
2022-08-08 02:42:29 +0430

    update

--- sections/Pos.jsx
+++ sections/Pos.jsx
@@ -6,7 +6,7 @@ const Pos = () => {
 			<div className="container">
 				<div className="row justify-content-md-center align-items-center">
 					<div className="col-9 text-center">
-						<h2 className="section-title">POS</h2>
+						<h2 className="section-title">Point of Sale System</h2>
 						<p className="section-desc  text-center">
 							The Landlord have started developing a unique and intuitive crypto <u>Point of Sale system</u>. This will allow bars, pubs, clubs and cafes to
 							successfully take orders, manage inventory and accept payment in various types of crypto currency including $LNDLRD.
--- sections/Store.js
+++ sections/Store.js
@@ -1,57 +1,63 @@
-import { Pagination } from 'swiper';
+import LinkArrow from '../components/LinkArrow';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper';
+
 import 'swiper/css';
 
-import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 const Store = () => {
 	return (
-		<section className="store">
+		<section className="store section">
 			<div className="container">
-				<div className="row align-items-center g-5">
-					<div className="col-6">
-						<h2 className="beers-title">Landlord Store</h2>
-						<p className="beers-desc">
-							We will create and sell some of the finest craft beers and ales. These will be available to purchase from the Online store with fiat or the crypto of
-							your choice. Profits will be used to buy back and burn LNDLRD.
-						</p>
-						<div className="beers-btns">
-							<a href="/">
-								All Products
-								<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" viewBox="0 0 16 16">
-									<path
-										fillRule="evenodd"
-										d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"
-									/>
-								</svg>
-							</a>
-						</div>
-					</div>
-					<div className="col-6">
-						<Swiper pagination={true} modules={[Pagination]} slidesPerView={1} spaceBetween={10} loop={true} className="mySwiper">
-							<SwiperSlide>
-								<div className="store-slider" style={{ backgroundImage: 'url(/files/img/store/01.jpg)' }}>
-									<h4>Landlord Short-sleeve unisex</h4>
-								</div>
-							</SwiperSlide>
-							<SwiperSlide>
-								<div className="store-slider" style={{ backgroundImage: 'url(/files/img/store/02.jpg)' }}>
-									<h4>Landlord Short-sleeve unisex</h4>
-								</div>
-							</SwiperSlide>
-							<SwiperSlide>
-								<div className="store-slider" style={{ backgroundImage: 'url(/files/img/store/03.jpg)' }}>
-									<h4>Landlord Short-sleeve unisex</h4>
-								</div>
-							</SwiperSlide>
-							<SwiperSlide>
-								<div className="store-slider" style={{ backgroundImage: 'url(/files/img/store/04.jpg)' }}>
-									<h4>Landlord Short-sleeve unisex</h4>
-								</div>
-							</SwiperSlide>
-						</Swiper>
-					</div>
+				<div className="d-flex align-items-center mb-5">
+					<h2 className="section-title m-0  me-auto">Landlord Store</h2>
+					<LinkArrow href="/" dir="right" className="link link-info">
+						All Products
+					</LinkArrow>
+				</div>
+			</div>
+
+			<div className="swiper-overflow-container">
+				<div className="container">
+					<Swiper navigation={true} slidesPerView={3} centeredSlides={false} modules={[Navigation]} spaceBetween={32} loop={false} className="mySwiper">
+						<SwiperSlide>
+							<div className="store-slider" style={{ backgroundImage: 'url(/files/img/store/01.jpg)' }}>
+								{/* <h4>Landlord Short-sleeve unisex</h4> */}
+							</div>
+						</SwiperSlide>
+						<SwiperSlide>
+							<div className="store-slider" style={{ backgroundImage: 'url(/files/img/store/02.jpg)' }}>
+								{/* <h4>Landlord Short-sleeve unisex</h4> */}
+							</div>
+						</SwiperSlide>
+						<SwiperSlide>
+							<div className="store-slider" style={{ backgroundImage: 'url(/files/img/store/03.jpg)' }}>
+								{/* <h4>Landlord Short-sleeve unisex</h4> */}
+							</div>
+						</SwiperSlide>
+						<SwiperSlide>
+							<div className="store-slider" style={{ backgroundImage: 'url(/files/img/store/04.jpg)' }}>
+								{/* <h4>Landlord Short-sleeve unisex</h4> */}
+							</div>
+						</SwiperSlide>
+						<SwiperSlide>
+							<div className="store-slider" style={{ backgroundImage: 'url(/files/img/store/02.jpg)' }}>
+								{/* <h4>Landlord Short-sleeve unisex</h4> */}
+							</div>
+						</SwiperSlide>
+						<SwiperSlide>
+							<div className="store-slider" style={{ backgroundImage: 'url(/files/img/store/03.jpg)' }}>
+								{/* <h4>Landlord Short-sleeve unisex</h4> */}
+							</div>
+						</SwiperSlide>
+						<SwiperSlide>
+							<div className="store-slider" style={{ backgroundImage: 'url(/files/img/store/04.jpg)' }}>
+								{/* <h4>Landlord Short-sleeve unisex</h4> */}
+							</div>
+						</SwiperSlide>
+					</Swiper>
 				</div>
 			</div>
 		</section>
