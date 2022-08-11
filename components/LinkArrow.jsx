757560922d0eb99e5b24c0ea0d663c115bdb1471
FrankHodler <frankhodler@gmail.com>
2022-08-11 22:30:44 +0430

    update styles

--- components/LinkArrow.jsx
+++ components/LinkArrow.jsx
@@ -1,11 +1,19 @@
-import { ArrowRightShort } from 'react-bootstrap-icons';
-
 const LinkArrow = ({ dir, href, className, children }) => {
+	const ArrowRightShort = () => {
+		return (
+			<svg xmlns="http://www.w3.org/2000/svg" width="32" height="16" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16">
+				<path
+					fillRule="evenodd"
+					d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"
+				/>
+			</svg>
+		);
+	};
 	return (
 		<a href={href} className={className}>
-			{dir === 'left' && <ArrowRightShort size="32" />}
+			{dir === 'left' && <ArrowRightShort />}
 			{children}
-			{dir === 'right' && <ArrowRightShort size="32" />}
+			{dir === 'right' && <ArrowRightShort />}
 		</a>
 	);
 };
