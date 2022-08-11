f519883e32f38cad23d0140569ee4305fa7a717c
FrankHodler <frankhodler@gmail.com>
2022-08-11 23:16:20 +0430

    fixing hero

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -2,20 +2,14 @@ const Hero = () => {
 	return (
 		<section className="hero">
 			<article className="container">
-				<div className="row">
-					<div className="col-md-6">
-						<h1 className="hero-title">
-							<span>World's first crypto</span> <span>pubs &amp; beers</span>
-						</h1>
-						<p className="hero-desc">
-							The Landlord Token ($LNDLRD) drives the whole Landlord ecosystem. A BEP-20 token that incorporates 3 fantastic protocols into it's smart contract.
-							Together, it's tokenomics power-up one truly unique token and will really drive forward the Pub industry.
-						</p>
-						<a className="hero-btn" href="/">
-							How To Buy
-						</a>
-					</div>
-				</div>
+				<h1 className="hero-title">World's first crypto pubs &amp; beers</h1>
+				<p className="hero-desc">
+					The Landlord Token ($LNDLRD) drives the whole Landlord ecosystem. A BEP-20 token that incorporates 3 fantastic protocols into it's smart contract. Together,
+					it's tokenomics power-up one truly unique token and will really drive forward the Pub industry.
+				</p>
+				<a className="hero-btn btn btn-brand" href="/">
+					How To Buy
+				</a>
 			</article>
 		</section>
 	);
