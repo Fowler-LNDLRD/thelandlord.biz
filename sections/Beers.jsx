894db494ab90a052dee4a637cbff3f4f5853ca67
FrankHodler <frankhodler@gmail.com>
2022-08-13 01:35:28 +0430

    update styels

--- sections/Beers.jsx
+++ sections/Beers.jsx
@@ -5,16 +5,16 @@ const Beers = () => {
 		<section className="beers section section-brand">
 			<div className="container">
 				<div className="row align-items-center">
-					<div className="col-md-6 mb-5 text-center text-md-start mb-md-0">
+					<div className="col-md-6 text-center text-md-start mb-5 mb-md-0">
 						<img className="beers-img" src="/files/img/beers/beer.png" />
 					</div>
-					<div className="col-md-6 ps-0 text-center text-md-start ps-md-1">
+					<div className="col-md-6 ps-0 ps-md-2">
 						<h2 className="section-title">Landlord Beers</h2>
-						<p className="section-desc mb-2">
+						<p className="section-desc mb-1">
 							We have formed a partnership with Crafty Brewery in the UK to release the first Landlord craft beer REKT which is an amazing Pilsner. This will be
 							available in several stores in the UK.
 						</p>
-						<p className="section-desc mb-2">
+						<p className="section-desc mb-1">
 							Additional partnerships in other countries will be established and additional beers will be launched and sold via the online store as well as bars
 							across the world.
 						</p>
