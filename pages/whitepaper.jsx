a6a557d3f4223a9ba047c10ade8036fd9e6e939d
FrankHodler <frankhodler@gmail.com>
2022-08-15 16:15:40 +0430

    fix whitepaper image

--- pages/whitepaper.jsx
+++ pages/whitepaper.jsx
@@ -1,20 +1,20 @@
+/* eslint-disable @next/next/no-img-element */
 import Link from 'next/link';
-import Image from 'next/image';
 
 const whitepaper = () => {
 	return (
 		<>
 			<section className="page page-whitepaper container">
-				<div className="row align-items-center my-auto">
-					<div className="col-md-6">
+				<div className="row align-items-center my-auto text-center text-md-start">
+					<div className="col-md-6 mb-5 mb-md-0">
 						<h1 className="page-whitepaper-title">Whitepaper</h1>
 						<div className="page-whitepaper-desc">Download the latest version of our whitepaper.</div>
 						<Link href="/">
 							<a className="btn btn-brand">Download</a>
 						</Link>
 					</div>
-					<div className="col-md-6">
-						<Image width="415" height="415" src="/files/img/certs/whitepaper.png" alt="Landlord Whitepaper" />
+					<div className="col-md-6 text-center">
+						<img className="page-whitepaper-img" width="320" height="320" src="/files/img/certs/whitepaper.png" alt="Landlord Whitepaper" />
 					</div>
 				</div>
 			</section>
