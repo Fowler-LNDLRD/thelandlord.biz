f87f9126ed247308a92e50f1c38ab17edf62bc11
FrankHodler <frankhodler@gmail.com>
2022-08-17 20:48:22 +0430

    error pages

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+const NotFound = () => {
+	return (
+		<div className="page">
+			<div className="container text-center">
+				<h1 className="page-title">404</h1>
+				<p className="page-desc">Page not found!</p>
+			</div>
+		</div>
+	);
+};
+export default NotFound;
---
+++ pages/500.js
@@ -0,0 +1,11 @@
+const NotFound = () => {
+	return (
+		<div className="page">
+			<div className="container text-center">
+				<h1 className="page-title">500</h1>
+				<p className="page-desc">Server error!</p>
+			</div>
+		</div>
+	);
+};
+export default NotFound;
