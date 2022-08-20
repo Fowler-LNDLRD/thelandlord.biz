d95c63eebc8731c7fe295e9c23854aa14ab40c44
FrankHodler <frankhodler@gmail.com>
2022-08-20 18:14:36 +0430

    fix shills

--- sections/home/Shill.jsx
+++ sections/home/Shill.jsx
@@ -9,15 +9,25 @@ import 'swiper/css/effect-cards';
 
 const Shill = () => {
 	const [shillers, setShillers] = useState(shills);
-	const [isLocal, setIsLocal] = useState(true);
+	const [total, setTotal] = useState(0);
+
+	const countTotal = (data) => {
+		let t = 0;
+		data.forEach((item) => {
+			t += item.count;
+		});
+		setTotal(t);
+	};
+
 	const getShillers = async () => {
 		try {
-			const users = await axios.get('https://landlord.army/api/board');
-			setShillers(users.data.shills);
-			setIsLocal(false);
+			const resp = await axios.get('https://landlord.army/api/board');
+			const data = resp.data.shills;
+			countTotal(data);
+			setShillers(data);
 		} catch (err) {
-			console.log(err.response.data.message);
-			setIsLocal(true);
+			console.log(err?.response?.data?.message);
+			countTotal(shills);
 		}
 	};
 
@@ -28,18 +38,16 @@ const Shill = () => {
 	return (
 		<section className="shill section">
 			<div className="container">
-				<div className="row align-items-center">
+				<div className="row align-items-center px-1 px-md-0">
 					<div className="col-md-6 col-lg-5">
-						<h2 className="section-title text-center text-md-start">Earn Landlord</h2>
-						<p className="section-desc text-center text-md-start">
+						<h2 className="section-title">Earn Landlord</h2>
+						<p className="section-desc">
 							Landlord Army is an app that we developed to enrich community engagement, and encourage its users to inviting more investors into The Landlord. You can
 							join the Landlord Army and earn $LNDLRD by just Tweeting about it.
 						</p>
-						<div className="text-center text-md-start">
-							<LinkArrow href="/" dir="right" className="link link-brand">
-								Join Now
-							</LinkArrow>
-						</div>
+						<LinkArrow href="/" dir="right" className="link link-brand">
+							Join Now
+						</LinkArrow>
 					</div>
 					<div className="col-md-6 col-lg-7 d-flex mt-3 mt-md-0">
 						<div className="shill-wrap">
@@ -50,7 +58,7 @@ const Shill = () => {
 											<path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
 										</svg>
 										<div className="shill-main-desc">
-											43,300,000 $LNDLRD <br /> earned today.
+											{(total * 5000).toLocaleString()} $LNDLRD <br /> earned so far.
 										</div>
 										<div className="shill-main-swipe">
 											<svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" viewBox="0 0 16 16">
@@ -75,8 +83,8 @@ const Shill = () => {
 												alt={item.username}
 											/>
 											<div className="shill-username">@{item.username}</div>
-											<div className="shill-points">{item.count} Points</div>
 											<div className="shill-lndlrd">{(item.count * 5000).toLocaleString()} $LNDLRD</div>
+											<div className="shill-points">{item.count} Points</div>
 										</div>
 									</SwiperSlide>
 								))}
