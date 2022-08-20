b0c5c668b9fb3e23948f3e0e3b95166c15f0601f
FrankHodler <frankhodler@gmail.com>
2022-08-20 17:28:29 +0430

    pos

--- sections/home/Pos.jsx
+++ sections/home/Pos.jsx
@@ -4,10 +4,10 @@ import POS from '../../components/POS';
 const Pos = () => {
 	return (
 		<section className="pos section section-dark">
-			<div className="container">
-				<h2 className="section-title text-center">Point of Sale System</h2>
-				<p className="section-desc text-center mb-5">
-					The Landlord have started developing a unique and intuitive crypto <u>Point of Sale system</u>. This will allow bars, pubs, clubs and cafes to successfully take
+			<div className="container px-3 px-md-2">
+				<h2 className="section-title text-start text-md-center">Point of Sale System</h2>
+				<p className="section-desc text-start text-md-center mb-3 mb-md-5">
+					The Landlord have started developing a unique and intuitive crypto Point of Sale system. This will allow bars, pubs, clubs and cafes to successfully take
 					orders, manage inventory and accept payment in various types of crypto currency including $LNDLRD.
 				</p>
 				<div className="pos-figure d-flex d-block d-md-none">
