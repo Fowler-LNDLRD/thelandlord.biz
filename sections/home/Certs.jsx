71a76b21254a53e6bf5c11d7a9f2b6a1c012b5ab
FrankHodler <frankhodler@gmail.com>
2022-08-21 00:10:35 +0430

    update

--- sections/home/Certs.jsx
+++ sections/home/Certs.jsx
@@ -6,25 +6,29 @@ const Certs = () => {
 	return (
 		<section className="certs section section-dark">
 			<div className="container">
-				<div className="row">
-					<div className="col-md-6 d-flex flex-column justify-content-center align-items-center">
-						<Book className="book-whitepaper" />
-						<div className="certs-item mb-4 mb-md-0 mt-5">
-							<h2 className="certs-title">Whitepaper</h2>
-							<div className="certs-desc">The latest version of our whitepaper.</div>
-							<LinkArrow className="link-brand" href="/" dir="right">
-								Download
-							</LinkArrow>
+				<div className="row g-5">
+					<div className="col-md-6 d-flex justify-content-center  justify-content-md-start">
+						<div className="cert-item d-md-block d-lg-flex g-0 align-items-center">
+							<Book className="book-whitepaper" />
+							<div className="certs-info ms-md-0 ms-lg-2 mt-3 mt-lg-0">
+								<h2 className="certs-title">Whitepaper</h2>
+								<div className="certs-desc d-none d-md-block">The latest version.</div>
+								<LinkArrow className="link-brand" href="/" dir="right">
+									Download
+								</LinkArrow>
+							</div>
 						</div>
 					</div>
-					<div className="col-md-6 d-flex flex-column justify-content-center align-items-center">
-						<Book className="book-audit" />
-						<div className="certs-item mb-4 mb-md-0 mt-5">
-							<h2 className="certs-title">Audit</h2>
-							<div className="certs-desc">Performed by Desert Finance</div>
-							<LinkArrow className="link-brand" href="/" dir="right">
-								Download
-							</LinkArrow>
+					<div className="col-md-6 d-flex justify-content-center  justify-content-md-start">
+						<div className="cert-item d-md-block d-lg-flex g-0 align-items-center">
+							<Book className="book-audit" />
+							<div className="certs-info ms-md-0 ms-lg-2 mt-3 mt-lg-0">
+								<h2 className="certs-title">Audit</h2>
+								<div className="certs-desc d-none d-md-block">Performed by Desert Finance.</div>
+								<LinkArrow className="link-brand" href="/" dir="right">
+									Download
+								</LinkArrow>
+							</div>
 						</div>
 					</div>
 				</div>
