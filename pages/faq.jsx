14ffd912fb9f4774134fd153cd33ec20fc3aff7f
FrankHodler <frankhodler@gmail.com>
2022-08-23 19:07:56 +0430

    faq

--- pages/faq.jsx
+++ pages/faq.jsx
@@ -1,15 +1,37 @@
+import Head from 'next/head';
 import HeadTag from '../components/HeadTag';
 import LandlordFAQ from '../sections/faq/LandlordFAQ';
 import GeneralFAQ from '../sections/faq/GeneralFAQ';
 import SlideFade from '../components/SlideFade';
+import faqGoogle from '../data/faqGoogle.json';
 
 const Faq = () => {
+	const faqEntity = faqGoogle.map((item) => {
+		return {
+			'@type': 'Question',
+			name: item.q,
+			acceptedAnswer: {
+				'@type': 'Answer',
+				text: item.a,
+			},
+		};
+	});
+	const faqData = {
+		'@context': 'https://schema.org',
+		'@type': 'FAQPage',
+		mainEntity: faqEntity,
+	};
+
 	return (
 		<>
 			<HeadTag
 				title={`FAQ`}
 				desc="Learn about Landlord Token, we have answered many frequently asked questions, and here you can find it. if not, join our telegram group."
 			/>
+			<Head>
+				<script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(faqData) }} />
+			</Head>
+
 			<SlideFade as="section" className="page page-faq container">
 				<h1 className="page-title text-center">FAQ</h1>
 				<div className="page-desc text-center page-desc-limit mb-5">You can find the answer to your question right here.</div>
