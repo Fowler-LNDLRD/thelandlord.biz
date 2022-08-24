2d12fa2ae9efeb1bd56374304c34f587b455c404
FrankHodler <frankhodler@gmail.com>
2022-08-24 19:38:40 +0430

    fix books

--- sections/home/Certs.jsx
+++ sections/home/Certs.jsx
@@ -1,7 +1,7 @@
-import Link from 'next/link';
 import LinkArrow from '../../components/LinkArrow';
 import Book from '../../components/Book';
 import SlideFade from '../../components/SlideFade';
+import Image from 'next/image';
 
 const Certs = () => {
 	return (
@@ -10,7 +10,17 @@ const Certs = () => {
 				<div className="row">
 					<SlideFade className="col-md-6 d-flex justify-content-center justify-content-md-start mb-5 mb-md-0">
 						<div className="cert-item d-md-block d-lg-flex align-items-center">
-							<Book className="book-whitepaper" />
+							<Book className="book-whitepaper">
+								<Image
+									alt="Landlord Token"
+									src="/img/home/whitepaper-cover.jpg"
+									width={200}
+									height={283}
+									objectFit="cover"
+									quality={100}
+									className="floating-img"
+								/>
+							</Book>
 							<div className="certs-info ms-md-0 ms-lg-2 mt-3 mt-lg-0">
 								<h2 className="certs-title">Whitepaper</h2>
 								<div className="certs-desc d-none d-md-block">The latest version.</div>
@@ -22,7 +32,9 @@ const Certs = () => {
 					</SlideFade>
 					<SlideFade delay={0.25} className="col-md-6 d-flex justify-content-center justify-content-md-start">
 						<div className="cert-item d-md-block d-lg-flex ps-md-1 align-items-center">
-							<Book className="book-audit" />
+							<Book className="book-audit">
+								<Image alt="Landlord Token" src="/img/home/audit-cover.jpg" width={200} height={264} objectFit="cover" quality={100} className="floating-img" />
+							</Book>
 							<div className="certs-info ms-md-0 ms-lg-2 mt-3 mt-lg-0">
 								<h2 className="certs-title">Audit</h2>
 								<div className="certs-desc d-none d-md-block">Performed by Desert Finance.</div>
