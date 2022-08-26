99e4ddddba9bba588f59192b4ee8c9d35e026b61
FrankHodler <frankhodler@gmail.com>
2022-08-26 23:34:04 +0430

    fix

--- pages/api/ball.js
+++ pages/api/ball.js
@@ -1,11 +1,22 @@
-const { scan } = require('bsc-tokenholdings');
 const axios = require('axios');
+var WAValidator = require('multicoin-address-validator');
 
 const fetchPrice = async () => {
 	try {
 		const { data: response } = await axios.get(process.env.COINGECKO_PRICE);
 		const data = response && response['0xd6da6491a6663b1d413715f4fd8edd74a4b47694']?.usd;
 		return data;
+	} catch (error) {
+		return null;
+	}
+};
+
+const fetchWallet = async (wallet) => {
+	try {
+		const { data: response } = await axios.get(
+			`https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=0xd6dA6491A6663B1d413715f4fD8eDD74a4b47694&address=${wallet}&tag=latest&apikey=P27GHAUQ5R3DBZD2CNIWSGBNVK4MR57YH5`
+		);
+		return response;
 	} catch (error) {
 		return null;
 	}
@@ -19,15 +30,21 @@ const handler = async (req, res) => {
 		// if no wallet
 		if (!req.query.wallet || req.query.wallet === '') return res.status(400).json({ message: 'Please add your wallet!' });
 
-		// if wallet is valid
-		const wallet = await scan(req.query.wallet, 'bnb');
-		if (wallet.error_msg === 'Invalid wallet address') return res.status(400).json({ message: 'Invalid wallet address!' });
+		var valid = WAValidator.validate(req.query.wallet, 'BNB');
+		if (!valid) return res.status(400).json({ message: `Not a valid BSC wallet address!` });
 
-		// if they have LNDLRD
-		if (!wallet.LNDLRD || !wallet.LNDLRD.balance) return res.status(400).json({ message: `You don't have $LNDLRD in your wallet!` });
+		// fetch wallet
+		const wallet = await fetchWallet(req.query.wallet);
 
-		const balance = Math.floor(wallet.LNDLRD.balance.replaceAll(',', ''));
+		// if wallet is empty
+		if (wallet.result === '0') return res.status(400).json({ message: `You don't have $LNDLRD in your wallet!` });
+
+		// get balance
+		const balance = Math.floor(Number(wallet.result) / 1e18);
+
+		// get price
 		const price = await fetchPrice();
+		if (!price) return res.status(500).json({ message: 'Can not get the price!' });
 
 		return res.status(200).json({ price, balance });
 	} catch (err) {
--- pages/ball.jsx
+++ pages/ball.jsx
@@ -94,7 +94,7 @@ const Calc = () => {
 				<div className="col-6">
 					<div className="form-item mb-1">
 						<div className="form-wallet">
-							<input value={wallet} onChange={walletHandler} name="wallet" type="text" className="form-control pe-3" />
+							<input value={wallet} onChange={walletHandler} name="wallet" placeholder="Wallet address" type="text" className="form-control pe-3" />
 							<button hidden={loading} onClick={walletChange} className="btn form-search">
 								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
 									<path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
