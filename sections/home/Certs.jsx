e46570215db9e0a2725a7de1c6fafcd0a53e3adc
FrankHodler <frankhodler@gmail.com>
2022-08-30 17:45:34 +0430

    certs translation

--- sections/home/Certs.jsx
+++ sections/home/Certs.jsx
@@ -3,7 +3,10 @@ import Book from '../../components/Book';
 import SlideFade from '../../components/SlideFade';
 import Image from 'next/image';
 
+import { useTranslations } from 'next-intl';
 const Certs = () => {
+	const t = useTranslations('home.certs');
+	const tc = useTranslations('common');
 	return (
 		<section className="certs section" id="whitepaper">
 			<div className="container">
@@ -12,7 +15,7 @@ const Certs = () => {
 						<div className="cert-item d-md-block d-lg-flex align-items-center">
 							<Book className="book-whitepaper">
 								<Image
-									alt="Landlord Token"
+									alt={tc('whitepaper')}
 									src="/img/home/whitepaper-cover.jpg"
 									width={200}
 									height={283}
@@ -22,10 +25,10 @@ const Certs = () => {
 								/>
 							</Book>
 							<div className="certs-info ms-md-0 ms-lg-2 mt-3 mt-lg-0">
-								<h2 className="certs-title">Whitepaper</h2>
-								<div className="certs-desc d-none d-md-block">The latest version.</div>
+								<h2 className="certs-title">{tc('whitepaper')}</h2>
+								<div className="certs-desc d-none d-md-block">{t('whitepaper')}</div>
 								<LinkArrow className="link-brand" href={process.env.WHITEPAPER} dir="right">
-									Download
+									{tc('download')}
 								</LinkArrow>
 							</div>
 						</div>
@@ -33,13 +36,13 @@ const Certs = () => {
 					<SlideFade delay={0.25} className="col-md-6 d-flex justify-content-center justify-content-md-start">
 						<div className="cert-item d-md-block d-lg-flex ps-md-1 align-items-center">
 							<Book className="book-audit">
-								<Image alt="Landlord Token" src="/img/home/audit-cover.jpg" width={200} height={264} objectFit="cover" quality={100} className="floating-img" />
+								<Image alt={tc('audit')} src="/img/home/audit-cover.jpg" width={200} height={264} objectFit="cover" quality={100} className="floating-img" />
 							</Book>
 							<div className="certs-info ms-md-0 ms-lg-2 mt-3 mt-lg-0">
-								<h2 className="certs-title">Audit</h2>
-								<div className="certs-desc d-none d-md-block">Performed by Desert Finance.</div>
+								<h2 className="certs-title">{tc('audit')}</h2>
+								<div className="certs-desc d-none d-md-block">{t('audit')}</div>
 								<LinkArrow className="link-brand" href={process.env.AUDIT} dir="right">
-									Download
+									{tc('download')}
 								</LinkArrow>
 							</div>
 						</div>
