627608d32234bfd6cd2356f58f943efd8e20d88e
FrankHodler <frankhodler@gmail.com>
2022-08-30 17:41:36 +0430

    earn translation

--- sections/home/Earn.jsx
+++ sections/home/Earn.jsx
@@ -5,8 +5,10 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { EffectCards } from 'swiper';
 import SlideFade from '../../components/SlideFade';
 import Image from 'next/image';
-
+import { useTranslations } from 'next-intl';
 const Shill = () => {
+	const t = useTranslations('home.earn');
+	const tc = useTranslations('common');
 	const [earnData, setEarnData] = useState(null);
 	useEffect(() => {
 		if (process.env.NODE_ENV !== 'development') {
@@ -30,15 +32,13 @@ const Shill = () => {
 				<div className="row align-items-center px-1 px-md-0">
 					<SlideFade className="col-md-6 col-lg-7 order-last ps-md-4">
 						<h2 className="section-title">
+							{}
 							{earnData?.earned ? earnData?.earned?.toLocaleString() : '140,000,000+'}
-							<br /> $LNDLRD earned
+							<br /> {t('title')}
 						</h2>
-						<p className="section-desc">
-							We developed an app to enrich community engagement, and encourage it&apos;s users to inviting more investors into The Landlord. You can join the
-							Landlord Army and earn $LNDLRD by just Tweeting about it.
-						</p>
+						<p className="section-desc">{t('desc')}</p>
 						<LinkArrow href={process.env.SHILL_URL} target="_blank" rel="noreferrer" dir="right" className="link link-brand">
-							Join Now
+							{tc('join_now')}
 						</LinkArrow>
 					</SlideFade>
 					<SlideFade y={0} x={0} delay={0.1} className="col-md-6 col-lg-5 d-flex mb-3 mb-md-0 pe-0 pe-md-4">
@@ -61,7 +61,9 @@ const Shill = () => {
 
 												<div className="shill-username">@{item.username}</div>
 												<div className="shill-lndlrd">{item.earned.toLocaleString()} $LNDLRD</div>
-												<div className="shill-points">{item.points.toLocaleString()} Points</div>
+												<div className="shill-points">
+													{item.points.toLocaleString()} {tc('points')}
+												</div>
 											</div>
 										</SwiperSlide>
 									))}
