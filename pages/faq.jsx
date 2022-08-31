1bdbeceba5621668d80ecb6df9b05ee88862bfe2
FrankHodler <frankhodler@gmail.com>
2022-08-31 12:24:16 +0430

    faq translation

--- pages/faq.jsx
+++ pages/faq.jsx
@@ -1,10 +1,12 @@
 import Head from 'next/head';
 import HeadTag from '../components/HeadTag';
-import LandlordFAQ from '../sections/faq/LandlordFAQ';
-import GeneralFAQ from '../sections/faq/GeneralFAQ';
 import SlideFade from '../components/SlideFade';
 import faqGoogle from '../data/faqGoogle.json';
 import { useTranslations } from 'next-intl';
+import Accordion from 'react-bootstrap/Accordion';
+import { useRouter } from 'next/router';
+import { en, de } from '../data/faq/';
+import parse from 'html-react-parser';
 
 const Faq = () => {
 	const t = useTranslations('faq');
@@ -24,22 +26,42 @@ const Faq = () => {
 		mainEntity: faqEntity,
 	};
 
+	const router = useRouter();
+	const { locale } = router;
+	const faq = locale === 'en' ? en : locale === 'de' ? de : [];
+
 	return (
 		<>
 			<HeadTag title={t('title')} desc={t('desc')} />
 			<Head>
 				<script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(faqData) }} />
 			</Head>
-
 			<SlideFade as="section" className="page page-faq container">
 				<h1 className="page-title text-center">{t('title')}</h1>
 				<div className="page-desc text-center page-desc-limit mb-5">{t('desc')}</div>
-
 				<div className="row align-items-center justify-content-center">
 					<div className="col-md-8">
-						<LandlordFAQ />
+						<Accordion defaultActiveKey="0" className="accordion-faq" flush>
+							{faq.landlord.map((item, index) => (
+								<Accordion.Item key={index} eventKey={index}>
+									<Accordion.Header>{item.q}</Accordion.Header>
+									<Accordion.Body>
+										<div>{parse(item.a)}</div>
+									</Accordion.Body>
+								</Accordion.Item>
+							))}
+						</Accordion>
 						<h4 className="mt-5 mb-2">{t('general_questions')}</h4>
-						<GeneralFAQ />
+						<Accordion defaultActiveKey="0" className="accordion-faq" flush>
+							{faq.general.map((item, index) => (
+								<Accordion.Item key={index} eventKey={index}>
+									<Accordion.Header>{item.q}</Accordion.Header>
+									<Accordion.Body>
+										<div>{parse(item.a)}</div>
+									</Accordion.Body>
+								</Accordion.Item>
+							))}
+						</Accordion>
 					</div>
 				</div>
 			</SlideFade>
