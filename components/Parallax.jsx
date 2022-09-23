d7884550988b44b609f8d8aa3ebea1b92ae60d6c
FrankHodler <frankhodler@gmail.com>
2022-09-23 14:26:55 +0330

    PARALLAX

--- components/Parallax.jsx
+++ components/Parallax.jsx
@@ -1,40 +1,39 @@
-import { useState, useRef, useEffect, ReactNode } from 'react';
-import { motion, useScroll, useTransform, useSpring, useReducedMotion } from 'framer-motion';
-
 const Parallax = ({ className, children, offset = 50, ariaHidden }) => {
-	const prefersReducedMotion = useReducedMotion();
-	const [elementTop, setElementTop] = useState(0);
-	const [clientHeight, setClientHeight] = useState(0);
-	const ref = useRef(null);
+	// 	import { useState, useRef, useEffect, ReactNode } from 'react';
+	// import { motion, useScroll, useTransform, useSpring, useReducedMotion } from 'framer-motion';
+	// const prefersReducedMotion = useReducedMotion();
+	// const [elementTop, setElementTop] = useState(0);
+	// const [clientHeight, setClientHeight] = useState(0);
+	// const ref = useRef(null);
 
-	const { scrollY } = useScroll();
+	// const { scrollY } = useScroll();
 
-	const initial = elementTop - clientHeight;
-	const final = elementTop + offset;
+	// const initial = elementTop - clientHeight;
+	// const final = elementTop + offset;
 
-	const yRange = useTransform(scrollY, [initial, final], [offset, -offset]);
-	const y = useSpring(yRange, { stiffness: 400, damping: 90 });
+	// const yRange = useTransform(scrollY, [initial, final], [offset, -offset]);
+	// const y = useSpring(yRange, { stiffness: 400, damping: 90 });
 
-	useEffect(() => {
-		// const element = ref.current;
-		// const onResize = () => {
-		// 	setElementTop(element.getBoundingClientRect().top + window.scrollY || window.pageYOffset);
-		// 	setClientHeight(window.innerHeight);
-		// };
-		// onResize();
-		// window.addEventListener('resize', onResize);
-		// return () => window.removeEventListener('resize', onResize);
-	}, [ref]);
+	// useEffect(() => {
+	// 	// const element = ref.current;
+	// 	// const onResize = () => {
+	// 	// 	setElementTop(element.getBoundingClientRect().top + window.scrollY || window.pageYOffset);
+	// 	// 	setClientHeight(window.innerHeight);
+	// 	// };
+	// 	// onResize();
+	// 	// window.addEventListener('resize', onResize);
+	// 	// return () => window.removeEventListener('resize', onResize);
+	// }, [ref]);
 
-	// Don't parallax if the user has "reduced motion" enabled
-	if (prefersReducedMotion) {
-		return <>{children}</>;
-	}
+	// // Don't parallax if the user has "reduced motion" enabled
+	// if (prefersReducedMotion) {
+	// 	return <>{children}</>;
+	// }
 
 	return (
-		<motion.div aria-hidden={ariaHidden} className={className}>
+		<div aria-hidden={ariaHidden} className={className}>
 			{children}
-		</motion.div>
+		</div>
 	);
 };
 
